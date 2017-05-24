67c44cddf2c3c59b154f5dffdd2e8b78b5bc580d
Joaquin Ignacio <Keen@Revolt.local>
2017-05-24 05:23:00 -0700

    load of first updates for testing

---
+++ assets/javascript/app.js
@@ -0,0 +1,78 @@
+// TRIVIA GAME PSEUDO CODE
+
+// Global variables:
+// 	Correct Answers
+// 	Incorrect Answers
+// 	Total questions 
+
+
+// Game Start- OnClick Start, will start game and start timer. 
+
+
+// Scope 1- Timer and timers condition that at zero the game will end. 
+
+// 	At timers end, the game will check for correct answers and provide a statistic
+
+
+// Scope 2- Questions are displayed into the HTML with radio buttons
+
+// 	variables
+// 		Array/Object. 
+// 		Questions variables are labeled q1, q2, q3, etc. 
+// 		Question answers are given a numerical value to be checked against. 
+// 			Example:
+// 				q1: answers[answer1, answer2, answer3, answer4], correctAnswer="actual answer"
+// 				etc. 
+
+// when timer expires, then the check-answer ffunction will run. 
+
+// 	it will check then push out the answers of the variables. 
+
+
+
+		//Why use a for-loop at all? just check each answer... 
+// 	for (var i=0, i < total questions, i++) {
+// 		if (q1 === correct answer){
+// 			correct answers ++
+// 		}
+// 		else {
+// 			incorrect answers ++
+// 		}
+		
+// 		unanswered questions = total questions less (correct + incorrect);
+
+// 	}
+
+
+
+// limit the set time outs, always clear the intervals, call backs end the game 
+
+
+
+
+
+
+// What is a group of Kittens called? 
+// 	Kindle
+
+// How many hours does a cat spend sleeping?
+// 12-16 hours
+
+// How many bones does a cat have?
+// 230 bones
+
+// What does a cat mean when they Hiss?
+// Defensiveness.
+
+// What percentage of genes are shared between a Tiger and a housecat?
+// 95.6%
+
+// What are cat breeders called?
+// Catteries
+
+// What percentage of cats respond to catnip?
+// 50%
+
+// When was the first known cat video recorded?
+// 1894
+
